f44c0dd92a4ffc88e5178dff5ea9815833bf043e
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-04-04 16:33:31 -0500

    c

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,14 @@
 import gdsfactory  as gf
+from pyglet.libs.win32 import com
+
+
+def create_component():
+    c = gf.Component()
+    return c
+
+def add_component(layout, component):
+    com_ref = layout << component
+    return  com_ref
 
 def taper(length, width1, width2, style):
     c = gf.components.taper(length=length, width1=width1, width2=width2, cross_section=style)
@@ -7,3 +17,5 @@ def taper(length, width1, width2, style):
     c.draw_ports()
     c.pprint_ports()
     c.show()
+
+
