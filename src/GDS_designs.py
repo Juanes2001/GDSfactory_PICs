c9107d593a1799fb2de6f1c540597b4ff17526cb
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-04-04 17:16:42 -0500

    c

--- src/GDS_designs.py
+++ src/GDS_designs.py
@@ -1,20 +1,33 @@
 import gdsfactory  as gf
+import functions as fun
 
 # Create the main component
-c = gf.Component()
+# c = gf.Component()
+#
+# # Create a straight waveguide directly inside the component
+# wg = gf.components.straight(length=1000, cross_section=gf.cross_section.strip(width=3.0))
+# wg_ref = c << wg  # Insert the waveguide into the component
+#
+# # Create a taper directly inside the component
+# taper = gf.components.taper(length=300, width1=3.0, width2=0.8, cross_section="rib")
+# taper_ref = c << taper  # Insert the taper into the component
+#
+# # Connect the taper to the output port of the waveguide
+# taper_ref.connect("o1", wg_ref.ports["o2"])
+#
+# # Add only two ports to the final structure
+# c.add_port("o1", port=wg_ref.ports["o1"])  # Input port from the waveguide
+# c.add_port("o2", port=taper_ref.ports["o2"])  # Output port from the taper
 
-# Create a straight waveguide directly inside the component
-wg = gf.components.straight(length=1000, cross_section=gf.cross_section.strip(width=3.0))
-wg_ref = c << wg  # Insert the waveguide into the component
+c = fun.create_component()
+wg = fun.str_line(1000,2, gf.cross_section.strip(width=3.0))
+wg_ref = c << wg
 
-# Create a taper directly inside the component
-taper = gf.components.taper(length=300, width1=3.0, width2=0.8, cross_section="rib")
-taper_ref = c << taper  # Insert the taper into the component
+taper = fun.taper(300,3.0,0.8,"rib")
+taper_ref = c << taper
 
-# Connect the taper to the output port of the waveguide
 taper_ref.connect("o1", wg_ref.ports["o2"])
 
-# Add only two ports to the final structure
 c.add_port("o1", port=wg_ref.ports["o1"])  # Input port from the waveguide
 c.add_port("o2", port=taper_ref.ports["o2"])  # Output port from the taper
 
@@ -22,3 +35,8 @@ c.add_port("o2", port=taper_ref.ports["o2"])  # Output port from the taper
 c.plot()
 c.show()
 c.pprint_ports()
+
+
+
+
+
--- src/functions.py
+++ src/functions.py
@@ -6,16 +6,16 @@ def create_component():
     c = gf.Component()
     return c
 
-def add_component(layout, component):
-    com_ref = layout << component
-    return  com_ref
-
-def taper(length, width1, width2, style):
-    c = gf.components.taper(length=length, width1=width1, width2=width2, cross_section=style)
+def taper(length, width1, width2, crss_section):
+    c = gf.components.taper(length=length, width1=width1, width2=width2, cross_section=crss_section)
     # el cross section es la forma
     # que adopta la seccion transversal, en este caso es cuadrado
-    c.draw_ports()
-    c.pprint_ports()
-    c.show()
+    return c
+
+def str_line(length, num_points, crss_section):
+    # Import the structure
+    c = gf.components.straight(length=length, npoints=num_points, cross_section=crss_section)
+    return c
+
 
 
